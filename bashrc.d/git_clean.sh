105b09f46fc0697cd0c9a83cb5515f3122c1c532
Sylvain <s.metayer@groupeonepoint.com>
2020-07-06 09:39:34 +0200

    update

--- bashrc.d/git_clean.sh
+++ bashrc.d/git_clean.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 git_branch_clean() {
+  git fetch -p
   for b in $(git branch -vv | grep disparue | awk '{print $1}'); do git branch -D "$b"; done
 }
 
 git_branch_list() {
+  git fetch -p
   git branch -vv | grep disparue | awk '{print $1}'
 }
 
---
+++ bashrc.d/ipv6.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ipv6_off() {
+  sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+  sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+}
+
+ipv6_on() {
+  sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
+  sudo sysctl -w net.ipv6.conf.default.disable_ipv6=0
+}
