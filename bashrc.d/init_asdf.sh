c2456abed3d6ead06f8dc23aabee64e3c47d1764
Sylvain <s.metayer@groupeonepoint.com>
2020-11-13 16:23:49 +0100

    add asdf init setup

--- bashrc.d/init_asdf.sh
+++ bashrc.d/init_asdf.sh
@@ -1,17 +1,39 @@
 #!/usr/bin/env bash
 
 function init_asdf() {
-    nodeVersion='15.2.0'
+    nodeVersion='14.15.0'
     starshipVersion='0.46.2'
+    terraformVersion='0.13.5'
+
     if ! command -v asdf &>/dev/null; then
         echo "COMMAND could not be found"
         exit
     fi
+
     asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
     bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
     asdf install nodejs $nodeVersion
     asdf global nodejs $nodeVersion
-    asdf plugin add starship
+
+    asdf plugin-add starship
     asdf install starship $starshipVersion
     asdf global starship $starshipVersion
+
+    asdf plugin-add terraform https://github.com/Banno/asdf-hashicorp.git
+    asdf install terraform $terraformVersion
+    asdf global terraform $terraformVersion
+
+    asdf plugin-add terragrunt https://github.com/lotia/asdf-terragrunt
+    asdf install terragrunt latest
+    # Maybe not working at first
+    asdf global terragrunt "$(asdf current terragrunt | xargs | cut -d" " -f 2)"
+
+    #asdf plugin-add docker-slim https://github.com/everpeace/asdf-docker-slim.git
+    #asdf install docker-slim $dockerSlimVersion
+    #asdf global docker-slim $dockerSlimVersion
+
+    asdf plugin-add shellcheck
+    asdf install shellcheck latest
+    # Maybe not working at first
+    asdf global shellcheck "$(asdf current shellcheck | xargs | cut -d" " -f 2)"
 }
