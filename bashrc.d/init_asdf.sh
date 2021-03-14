0315863c4245d75de102b0ba6a8286a05c9c47c2
Sylvain <github@sylvainmetayer.fr>
2021-03-14 20:38:02 +0100

    update asdf script

--- bashrc.d/init_asdf.sh
+++ bashrc.d/init_asdf.sh
@@ -24,20 +24,27 @@ function init_asdf() {
     asdf global terraform $terraformVersion
 
     asdf plugin-add terragrunt https://github.com/lotia/asdf-terragrunt
-    asdf install terragrunt latest
-    # Maybe not working at first
-    asdf global terragrunt "$(asdf current terragrunt | xargs | cut -d" " -f 2)"
+    asdf install terragrunt 0.28.12
+    asdf global terragrunt 0.28.12
 
+    # once https://github.com/everpeace/asdf-docker-slim/pull/3 is merged, uncomment
     #asdf plugin-add docker-slim https://github.com/everpeace/asdf-docker-slim.git
-    #asdf install docker-slim $dockerSlimVersion
-    #asdf global docker-slim $dockerSlimVersion
+    #asdf install docker-slim 1.32.0
+    #asdf global docker-slim 1.32.0
 
     asdf plugin-add shellcheck
-    asdf install shellcheck latest
-    # Maybe not working at first
-    asdf global shellcheck "$(asdf current shellcheck | xargs | cut -d" " -f 2)"
+    asdf install shellcheck 0.7.1
+    asdf global shellcheck 0.7.1
 
     asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
     asdf install helm 3.4.1
     asdf global helm 3.4.1
+
+    asdf plugin add ytt https://github.com/sylvainmetayer/asdf-ytt
+    asdf install ytt 0.31.0
+    asdf global ytt 0.31.0
+
+    asdf plugin-add eksctl
+    asdf install eksctl 0.40.0
+    asdf global eksctl 0.40.0
 }
