59a88e98cd47c2f2a61319f0a4debb0845316726
Sylvain METAYER <s.metayer@groupeonepoint.com>
2022-03-21 17:54:18 +0100

    feat: allow load env from custom file

--- ansible/bashrc.d/load_env.sh
+++ ansible/bashrc.d/load_env.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 load_env() {
-  if [ -f .env ]; then
+  file=${1:-.env}
+  if [ -f "$file" ]; then
     # We do not want to quote variable or they will assigned as one variable only.
     # shellcheck disable=SC2046
-    export $(sed 's/#.*//g' < .env | xargs)
+    export $(sed 's/#.*//g' < "$file" | xargs)
   else
-    echo "No .env file found in $(pwd)"
-    exit 1
+    echo "No ${file} file found in $(pwd)"
   fi
 }
