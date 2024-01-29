f7d098823ad8517f649ddb905e622dfc4a5e6d6c
Sylvain <--global>
2024-01-29 09:10:24 +0100

    update

--- roles/gop/files/gop.sh
+++ roles/gop/files/gop.sh
@@ -31,7 +31,8 @@ set_aws_creds_tf () {
 RUST_VERSION=1.70.0
 test -f $HOME/.asdf/install/rust/$RUST_VERSION/env &&  source /home/s.metayer/.asdf/installs/rust/$RUST_VERSION/env
 
-test -d $HOME/.asdf/installs/gcloud && source /home/s.metayer/.asdf/installs/gcloud/430.0.0/path.bash.inc
-test -d $HOME/.asdf/installs/gcloud && source /home/s.metayer/.asdf/installs/gcloud/430.0.0/completion.bash.inc
+test -d $HOME/.asdf/installs/gcloud && source /home/s.metayer/.asdf/installs/gcloud/$(gcloud --version | head -n 1 | cut -d" " -f 4)/path.bash.inc
+test -d $HOME/.asdf/installs/gcloud && source /home/s.metayer/.asdf/installs/gcloud/$(gcloud --version | head -n 1 | cut -d" " -f 4)/completion.bash.inc
+
 export USE_GKE_GCLOUD_AUTH_PLUGIN=False
 
